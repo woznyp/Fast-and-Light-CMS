71d2d409225306e0a5b32a13b2c0d6d040b2528b
Paweł Woźny <pawelpwozny@gmail.com>
2014-10-25 21:01:24 +0200

    Unit tests

--- unitTests/tools/RequireManagerTest.php
+++ unitTests/tools/RequireManagerTest.php
@@ -1,9 +1,44 @@
 <?php
 require_once(__DIR__ . '/../../tools/RequireManager.php');
 
-class RequireManagerTest{
+
+/**
+ * Uncomment return statements in functions in that class
+ */
+class RequireManagerTest extends PHPUnit_Framework_TestCase{
+    public function __construct() {
+        RequireManager::main();
+    }
     
-    public function testRequireClass(){
-        assert(RequireManager::requireClass('test', 'test') === Exception);
+    /**
+     * @expectedException Exception
+     */
+    public function testRequireClass1(){
+        RequireManager::requireClass('test', 'test');
+    }
+    
+    /**
+     * @expectedException Exception
+     */
+    public function testRequireClass2(){
+        RequireManager::requireClass('test', 'include');
+    }
+    
+    /**
+     * @expectedException Exception
+     */
+    public function testRequireClass3(){
+        RequireManager::requireClass('MainView', 'test');
+    }
+    
+    public function testRequireClass4(){
+        $this->assertEquals('string',gettype(RequireManager::requireClass('MainView', 'require_once')));
+    }
+    
+    /**
+     * @expectedException Exception
+     */
+    public function testRequireClass5(){
+        RequireManager::requireClass('MainControler', 'require_once');
     }
 }
